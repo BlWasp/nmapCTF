7ed4791e90ab55ade7176edbef1843c3d137fdc4
BlackWasp <35003340+BlWasp@users.noreply.github.com>
2020-01-24 14:50:12 +0100

    Add files via upload
    The main script

---
+++ scriptNmap.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+ip="${1}"
+portL=""
+
+#First scan to detecte all the open ports
+nmap -p- --min-rate 10000 $ip > firstScan
+
+while read line
+do
+	if [[ ${line:0:1} =~ [0-9] ]]
+	then
+		portL="${portL}${line%%/*},"
+	fi
+done < firstScan
+echo $portL
+
+#Second scan to scan with default scans only the open ports
+nmap -p $portL -sC -sV $ip
